a61ee7762c8abd672aad1eb3c1d737b915573b8f
m666362 <mdrayhan9464@gmail.com>
2021-06-22 15:13:25 +0600

    Updates

---
+++ precache-manifest.b707ef2f63f42228e1a40f64751bbdcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b269d7b1eced507f392279eb32cadf5",
+    "url": "/React-to-do-app/index.html"
+  },
+  {
+    "revision": "489966f59deb0d7639b8",
+    "url": "/React-to-do-app/static/css/main.52fc023a.chunk.css"
+  },
+  {
+    "revision": "fe9dffdc852f2043568c",
+    "url": "/React-to-do-app/static/js/2.ff30aee7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React-to-do-app/static/js/2.ff30aee7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489966f59deb0d7639b8",
+    "url": "/React-to-do-app/static/js/main.392c262b.chunk.js"
+  },
+  {
+    "revision": "ecb1860a15b8cf06655a",
+    "url": "/React-to-do-app/static/js/runtime-main.64235190.js"
+  }
+]);
